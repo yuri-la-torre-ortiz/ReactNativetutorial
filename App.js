c09ab70ae93b0c52b6c3c7534b91aa73255eab6a
Yuri La Torre Ortiz <yuri.la.torre.ortiz@protonmail.ch>
2020-10-01 12:31:55 +0200

    Set up basic layout

--- App.js
+++ App.js
@@ -3,21 +3,14 @@ import React, { useState } from 'react';
 import { Button, StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
-  const [ outputText, setOutputText ] = useState('Open up App.js to start working on your app!');
   return (
-    <View style={styles.container}>
-      <Text>{outputText}</Text>
-      <Button title="Change text" onPress={() => setOutputText('The text changed!')}/>
-      <StatusBar style="auto" />
+    <View>
+    <View>
+    ....
+    </View>
+    <View>
+
+    </View>
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
