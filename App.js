699a2692f9628a262946ece5777ef8b69373fd2e
Yuri La Torre Ortiz <yuri.la.torre.ortiz@protonmail.ch>
2020-10-04 13:23:37 +0200

    Finish flexbox module

--- App.js
+++ App.js
@@ -12,44 +12,6 @@ export default function App() {
         />
         <Button title="ADD" />
       </View>
-      <View 
-        style={{ 
-          flexDirection: 'row',
-          width: '80%', 
-          height: 300,
-          justifyContent: 'space-between',
-          alignItems: 'stretch' }}
-          >
-        <View
-          style={{
-            backgroundColor: 'blue',
-            justifyContent: 'center',
-            alignItems: 'center',
-            flex: 1
-          }}
-        >
-          <Text>1</Text>
-        </View>
-              <View
-          style={{
-            backgroundColor: 'orange',
-            justifyContent: 'center',
-            alignItems: 'center',
-            flex: 2
-          }}
-        >
-          <Text>2</Text>
-        </View>
-              <View
-          style={{
-            backgroundColor: 'red',
-            justifyContent: 'center',
-            alignItems: 'center'
-          }}
-        >
-          <Text>3</Text>
-        </View>
       </View>
-    </View>
   );
 }
