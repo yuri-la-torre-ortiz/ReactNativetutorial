a8d73c1afc68934387de198c6f506cc6278c8456
Yuri La Torre Ortiz <yuri.la.torre.ortiz@protonmail.ch>
2020-10-04 14:06:49 +0200

    Reformat using Stylesheet

--- App.js
+++ App.js
@@ -1,21 +1,31 @@
-import React, { useState } from 'react';
-import { Button, StyleSheet, Text, TextInput, View } from 'react-native';
+import React, { useState } from "react";
+import { Button, StyleSheet, Text, TextInput, View } from "react-native";
 
 export default function App() {
   return (
-    <View style={{ padding: 50 }}>
-      <View style={{ flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center' }}>
-        <TextInput 
-          placeholder="Course Goals" 
-          style={{ width: '75%', borderColor: 'black', borderWidth: 1, padding: 10 }} 
-
-        />
+    <View style={styles.screen}>
+      <View style={styles.inputContainer}>
+        <TextInput placeholder="Course Goals" style={styles.input} />
         <Button title="ADD" />
       </View>
-      </View>
+    </View>
   );
 }
 // Use a stylesheet instead of inline styling
 const styles = StyleSheet.create({
-
+  //any object name can be utilized
+  screen: {
+    padding: 50,
+  },
+  inputContainer: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+  },
+  input: {
+    width: "75%",
+    borderColor: "black",
+    borderWidth: 1,
+    padding: 10,
+  },
 });
